22307a28a5707ff3f0b6e2a8538c5d1ad138c924
sininetapja <mart.vesinurm@gmail.com>
2017-01-02 11:14:46 +0200

    chirstmas changes
    white box approaches Trump, can only jump once, and then again only
    when landed

---
+++ src/jumping/Obstacle.scala
@@ -0,0 +1,5 @@
+package jumping
+
+class Obstacle(var x: Int, var y: Int, val shape: String) {
+
+}
--- src/jumping/Window.scala
+++ src/jumping/Window.scala
@@ -1,6 +1,7 @@
 package jumping
 import scala.math._
 import processing.core._
+import scala.collection.mutable.Buffer
 
 object Window extends PApplet{
   def main(args:Array[String]) {
@@ -18,36 +19,50 @@ class Window extends PApplet {
   val bgImg = loadImage("src/jumping/BG.png")
   val trumpImg = loadImage("src/jumping/trump.png")
   var playerIcon = new Player
+  val obstacles = Buffer[Obstacle]()
+ 
+  
   //override settings
   def settings() = {
     size(areaWidth,areaHeight)
   }
+  
+  
   //main setup
   override def setup(): Unit = {
     size(areaWidth, areaHeight)
-    background(0)
+    background(bgImg)
     noStroke()
   }
 
   
   override def mousePressed() = { //Change to when in contact with platform
+    if(vy == 20) {
      vy = -15
+    }
    }
+  
   
   //draw runs 60 time per second
    var vy = 0
+   var sx = 0
   override def draw(): Unit = {
+     
    if(gameState == 1) {
-    clear()
-    fill(255)
-    if(playerIcon.xAxis > areaWidth) {
-      playerIcon.xAxis = 0
-    }
+    clear() 
     vy = min(vy+1, 20)
     playerIcon.yAxis = min(250,playerIcon.yAxis + vy)
+   
     playerIcon.xAxis += 0
-    image(bgImg, 0,0,640,320)
+   // image(bgImg, 0,0,640,320)
     image(trumpImg, playerIcon.xAxis, playerIcon.yAxis, 50, 50)
+    
+    
+    //TESTIKUUTIO
+    rect(obstacles(0).x+200-sx,obstacles(0).y,50,50)
+    sx += 2
+    if(obstacles(0).x+200-sx == playerIcon.xAxis && obstacles(0).y == playerIcon.yAxis) gameState = 0
+
     }
   }
 }
