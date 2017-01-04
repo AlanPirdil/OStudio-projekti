d626af7f8059f1e7c612f9577ea1bc3640962b60
sininetapja <mart.vesinurm@gmail.com>
2017-01-04 18:50:44 +0200

    SoundEffects class added

---
+++ src/jumping/SoundEffects.scala
@@ -0,0 +1,14 @@
+package jumping
+import ddf.minim._
+
+class SoundEffects {
+   
+  val effect = new Minim(Window)
+  val effect1 = effect.loadFile("???")
+  val currentEffect = ???
+  
+  def playEffect() = {
+   ???//  currentEffect.play()
+  }
+  
+}
