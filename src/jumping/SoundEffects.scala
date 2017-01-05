451a632e39a585e11445abb6d2158d03a0f83d15
sininetapja <mart.vesinurm@gmail.com>
2017-01-05 13:58:23 +0200

    SoundEffects

--- src/jumping/SoundEffects.scala
+++ src/jumping/SoundEffects.scala
@@ -4,11 +4,13 @@ import ddf.minim._
 class SoundEffects {
    
   val effect = new Minim(Window)
-  val effect1 = effect.loadFile("???")
-  val currentEffect = ???
+  private val effect1 = effect.loadFile("src/trumpEffects/jump.wav")
+  private val effect2 = effect.loadFile("src/trumpEffects/out.wav")
+  private val effect3 = effect.loadFile("src/trumpEffects/great_again.wav")
   
-  def playEffect() = {
-   ???//  currentEffect.play()
-  }
+  def play1() = effect1.play()
   
+  def play2() = effect2.play()
+  
+  def play3() = effect3. play()
 }
