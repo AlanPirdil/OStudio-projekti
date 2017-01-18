5f19563d24e774a9abecaed6d27d7fa2fe44f614
sininetapja <mart.vesinurm@gmail.com>
2017-01-18 13:25:48 +0200

    VictorySong

--- src/jumping/Music.scala
+++ src/jumping/Music.scala
@@ -7,11 +7,19 @@ class Music {
 
   val song1 = music.loadFile("src/music/alone.wav")
   val song2 = music.loadFile("src/music/eskimot.wav")
-  val song3 = music.loadFile("src/music/yep.mp3")
-  val song4 = music.loadFile("src/music/joystick.wav")
-  val song5 = music.loadFile("src/music/get_in.wav")
+  val song3 = music.loadFile("src/music/joystick.wav")
+  val song4 = music.loadFile("src/music/get_in.wav")
+  
+  val wallSong = music.loadFile("src/music/yep.mp3")
+  val victorySong = music.loadFile("src/music/mexico.wav")
+  
   
   var currentSong = song1
+  
+  def playVictory() = {
+    victorySong.play()
+    victorySong.rewind()
+  }
   
   def nextSong() = {
     currentSong.pause()
@@ -19,8 +27,7 @@ class Music {
     if(currentSong == song1) currentSong = song2
     else if(currentSong == song2) currentSong = song3
     else if(currentSong == song3) currentSong = song4
-    else if(currentSong == song4) currentSong = song5
-    else if(currentSong == song5) currentSong = song1
+    else if(currentSong == song4) currentSong = song1
   }
 
   def gameMusic() = {
